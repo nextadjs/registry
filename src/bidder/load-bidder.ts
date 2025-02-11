f6b5e47b8399889a255f44f0fccba794ad38c496
Kai Miyamoto <kaihogehoge@gmail.com>
2025-02-12 06:16:24 +0900

    fix: Correctly import default export for bidder specification

--- src/bidder/load-bidder.ts
+++ src/bidder/load-bidder.ts
@@ -4,7 +4,7 @@ import { Bidder } from "./bidder";
 
 export const loadBidder = async (name: string, runtime: Runtime) => {
   // TODO: 適切なエラーハンドリング
-  const spec = (await import(`@bidders/${name}/${runtime}`)) as BidderSpec;
+  const spec = (await import(`@bidders/${name}/${runtime}`)).default as BidderSpec;
   const config = await import(`@bidders/${name}/bidder.json`);
   return new Bidder(config, spec);
 };
