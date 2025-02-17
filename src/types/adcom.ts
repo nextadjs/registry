843b6a458b3a13bc0e6ee802a8f9908f33342806
Kai Miyamoto <kaihogehoge@gmail.com>
2025-02-17 12:20:51 +0900

    feat: Add AdCOMAd and AdCOMPlacement interfaces for improved type definitions

--- src/types/adcom.ts
+++ src/types/adcom.ts
@@ -1,4 +1,12 @@
 import type { App, Context, Dooh, Site } from "iab-adcom/context";
+import type { Ad } from "iab-adcom/media";
+import type { Placement } from "iab-adcom/placement";
+
+export interface AdCOMAd extends Ad {}
+
+export interface AdCOMPlacement extends Placement {
+  tagid: string;
+}
 
 export interface AdCOMContext extends Context {}
 
