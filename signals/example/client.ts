47a02850827975fa64efd83fe800e4adaead3e20
Kai Miyamoto <kaihogehoge@gmail.com>
2025-02-27 18:57:33 +0900

    feat: Update SignalClientIntegration to include a new Data interface for enhanced data handling

--- signals/example/client.ts
+++ signals/example/client.ts
@@ -2,11 +2,22 @@ import type { SignalClientIntegration } from "@/signal";
 import type { DefaultParams } from "@/types";
 
 interface Params extends DefaultParams {
-    site: number;
-};
+  site: number;
+}
 
-const integration: SignalClientIntegration<Params> = {
+interface Data {
+  hello: "world!";
+}
 
+const integration: SignalClientIntegration<Data, Params> = {
+  async collect() {
+    return {
+      data: {
+        hello: "world!",
+      },
+      asyncCollections: [],
+    };
+  },
 };
 
 export default integration;
